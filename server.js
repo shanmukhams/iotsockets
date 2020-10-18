ce0dae59bdfe4f89cf8388d05f720160c6a9da63
shanmukhams <37494779+shanmukhams@users.noreply.github.com>
2020-10-18 15:53:31 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/ui/index.html');
+});
+
+io.on('connect', (socket) => {
+    console.log('a user connected')
+    socket.send('Hello from server')
+    socket.emit('greetings', 'Hello from server event');
+      
+    socket.on('message', (data) => {
+        console.log(data);
+    });
+
+    socket.on('salutations', (data)=>{
+        console.log(data)
+    })
+})
+
+http.listen(3000, () => {
+    console.log('listening on 3000')
+})
